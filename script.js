dc0a9b48d5ee388e74ecdce540e9c4248e16f5eb
deeptesh-rout <153610471+deeptesh-rout@users.noreply.github.com>
2024-06-23 22:45:19 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+function show_clock(){
+
+    let h = document.getElementsByClassName('hr')[0];
+    let m = document.getElementsByClassName('mn')[0];
+    let s = document.getElementsByClassName('ss')[0];
+
+    let date = new Date(); 
+
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+
+    h.style.transform = `rotate(${30 * hours + minutes/2}deg)`;
+    m.style.transform = `rotate(${6 * minutes}deg)`;
+    s.style.transform = `rotate(${6 * seconds}deg)`;
+
+    let sound = new Audio('sound.mp3');
+    sound.play();
+}
+
+
+setInterval(show_clock, 1000);
